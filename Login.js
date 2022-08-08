bc0d43ebed40a94dc6dd4bb8270e8421542f46e7
Ekwonu Kenchukwu <kenzemadrt@gmail.com>
2022-08-08 20:16:11 +0100

    First Commit

---
+++ Login.js
@@ -0,0 +1,8 @@
+
+const submit=document.getElementById("login-btn")
+const handleSubmit=(event)=>{
+    event.preventDefault()
+
+console.log("Successfully Logged In")
+} 
+submit.addEventListener("click",handleSubmit)
