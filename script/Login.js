c2be3cd0b1024b45f78e7802362e9dee4b30cae1
Victory Fatoyinbo <vicfatoyinbo@gmail.com>
2022-08-09 17:29:38 +0100

    Made some style changes and temporarily disabled preventDefault to allow login to dashboard work without authentication

--- script/Login.js
+++ script/Login.js
@@ -1,8 +1,8 @@
 
 const submit=document.getElementById("login-btn")
-const handleSubmit=(event)=>{
-    event.preventDefault()
+// const handleSubmit=(event)=>{
+//     event.preventDefault()
 
-console.log("Successfully Logged In")
-} 
+// console.log("Successfully Logged In")
+// } 
 submit.addEventListener("click",handleSubmit)
